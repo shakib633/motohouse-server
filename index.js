d0f3e53ae5c3aff2838330429850a1fce5c0a7a4
shakiv633 <shakibshuvo633@gmail.com>
2022-05-08 23:43:59 +0600

    create api

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const app= express();
+const cors=require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 const port=process.env.PORT || 5000;
 
-app.get('/', (req,res)=>{
-    res.send()
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ph97a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+           await client.connect();
+           const dataCollection=client.db('MotoHouse').collection('products');
+         
+           app.get('/products', async(req, res )=>{
+            const  query = {};
+            const cursor = dataCollection.find(query);
+            const products= await cursor.toArray();
+            res.send(  products);
+           })
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir)
+
+
+app.get('/',(req,res) => {
+    res.send('run moto')
 });
 
-app.get('/products', (req, res)=>{
-    res.send('show my data')
-})
-
-app.listen(port, ()=>{
-    console.log('listing to port ', port);
+app.listen(port,()=>{
+    console.log('run server ', port);
 })
