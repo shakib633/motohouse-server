af6b38a3879a37a1ef25084a524de609324e8bf6
shakiv633 <shakibshuvo633@gmail.com>
2022-05-19 14:41:14 +0600

    post complete

--- index.js
+++ index.js
@@ -48,6 +48,33 @@ async function run() {
             
         });
 
+        app.put('/product/:id', async(req, res)=>{
+            const id=req.params.id;
+           const newQuantity=req.body;
+           const filter={_id: ObjectId(id)};
+           const option={upsert: true};
+           const updated={
+               $set:{
+                   quantity: newQuantity.quantity
+               }
+           };
+           const result=await dataCollection.updateOne(option,updated,filter)
+           res.send(result);
+        })
+        app.put('/product/:id', async(req, res)=>{
+            const id=req.params.id;
+           const newQuantity=req.body;
+           const filter={_id: ObjectId(id)};
+           const option={upsert: true};
+           const updated={
+               $set:{
+                   quantity: newQuantity.quantity
+               }
+           };
+           const result=await dataCollection.updateOne(option,updated,filter)
+           res.send(result);
+        })
+
     }
     finally{
 
